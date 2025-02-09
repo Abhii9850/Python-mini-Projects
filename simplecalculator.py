256d511dfb89314e848de1312e67c7999df967a0
ABHISHEK MUSALE <178720540+Abhii9850@users.noreply.github.com>
2025-02-09 21:40:26 +0530

    Add files via upload

---
+++ simplecalculator.py
@@ -0,0 +1,11 @@
+a = int(input("enter a number"))
+b = int(input("enter a number"))
+c = input("enter any operator bet'n : +,-,*,/")
+if c =="+":
+    print("the result is : ",a+b)
+elif c == "-" :
+    print("the result is : ",a-b)
+elif c == "* ":
+    print("the result is : ",a*b)
+else :
+    print("the result is : ",a/b)
